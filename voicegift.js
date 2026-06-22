b1c8354d0feb69dac6e42b5e873d99ce83710e19
Nick <nickjacklin99@web.de>
2026-06-22 14:57:01 +0200

    race conditions login

--- voicegift.js
+++ voicegift.js
@@ -189,6 +189,10 @@ function bindLogin(){
     btn.textContent = '…';
     btn.disabled    = true;
     try {
+      /* Sign out anonymous user first so it doesn't interfere */
+      if(auth.currentUser && auth.currentUser.isAnonymous){
+        await signOut(auth);
+      }
       const timeout = new Promise((_, reject) =>
         setTimeout(() => reject(Object.assign(new Error('timeout'), {code:'timeout'})), 12000)
       );
@@ -1138,7 +1142,8 @@ function bindAll(){
 }
 
 onAuthStateChanged(auth, user => {
-  if(user){
+  const isAdmin = user && !user.isAnonymous;
+  if(isAdmin){
     showView('projects');
     loadProjects();
   } else {
